65dd6979af48ae023e385300b4727163e42667c2
btnz-k <github@elleryweber.com>
2020-01-28 14:43:31 -0800

    progress

--- install.sh
+++ install.sh
@@ -19,9 +19,9 @@ NOTICE
 sleep 3
 
 # Make Critical Folders
-mkdir -p ~/.screen/{logs,caps} ~/client/recon /opt/multi/{stage,data,logs} /opt/multi/var/install
-chmod 775 ~/.screen/{logs,caps} ~/client/recon /opt/multi/{stage,data,logs} /opt/multi/var/install
-chown 1000:1000 ~/.screen/{logs,caps} ~/client/recon /opt/multi/{stage,data,logs} /opt/multi/var/install
+mkdir -p ~/.screen/{logs,caps} ~/client/recon 
+chmod 775 ~/.screen/{logs,caps} ~/client/recon
+chown -R 1000:1000 ~/.screen/{logs,caps} ~/client/recon
 
 tee <<-EOF
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -39,81 +39,29 @@ tee <<-EOF
 
 EOF
 
+ansible-playbook ./installPackages.yml
 
 tee <<-EOF
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-⌛  Updating the Server - Please Standby 
+⌛  Cloning Tools - Please Standby 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 EOF
 
-
-
-############################################################################################
-
+ansible-playbook ./cloneTools.yml
 
 tee <<-EOF
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-✅ Finished - Basic Updates
+⌛  Configuring Tools - Please Standby 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 EOF
 
-ansible-playbook /opt/multi/stage/clone.yml
-bash /opt/multi/stage/cloner/solo/update.sh
-
-
-
-
-
-# Copy Starting Commands for PGBlitz
-path="/pg/stage/alias"
-cp -t /bin $path/plexguide $path/pg $path/pgblitz
-
-# Verifying the Commands Installed
-tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-⌛  Verifiying Started Commands Installed via @ /bin
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-
-# Installation fails if the pgblitz command is not in the correct location
-file="/bin/pgblitz"
-if [ ! -e "$file" ]; then
+ansible-playbook ./setupTools.yml
 
 tee <<-EOF
-
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-⛔️  WARNING! The PGBlitz Installer Failed ~ http://pgblitz.wiki
+✅ Finished!
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-Please Reinstall PGBlitz by running the Command Again! This step is to
-ensure that everything is working prior to the install!
-Ensure that you utilizing the correct versions of linux as described on
-the installation page!
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EXITING!!!
-EOF
-exit
-fi
 
-# If nothing failed, notify the user that installation passed!
-tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-✅️  PASSED! The PGBlitz Command Installed! ~ http://pgblitz.wiki
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-
-# creates a blank var; if this executes; pgblitz will go through the install process
-touch /pg/var/new.install
-
-chmod 775 /bin/plexguide /bin/pgblitz /bin/pg
-chown 1000:1000 /bin/plexguide /bin/pgblitz /bin/pg
-
-tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-↘️  Start AnyTime By Typing >>> pgblitz [or] plexguide [or] pg
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 EOF
